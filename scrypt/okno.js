6508abef955869ae2ed15681820947b73c8c00db
dimasik8m <147922372+dimasik8m@users.noreply.github.com>
2024-06-19 19:40:16 +0300

    Climat_system

---
+++ scrypt/okno.js
@@ -0,0 +1,21 @@
+
+// Открытие всплывающего окна
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+document.querySelector('.close-btn').addEventListener('click', closePopup);
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('popup')) {
+        closePopup();
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    showPopup();
+});
